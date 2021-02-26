01ccd3f073c6612a923a21e78462a29aec1d35b5
JulienLaupretre <julien.laupretre@u-psud.fr>
2021-02-26 17:32:06 +0100

    user search + modif_v1

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import {Subject} from 'rxjs';
-import firebase from "firebase/app";
 import { HttpClient } from '@angular/common/http'
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +10,12 @@ import { HttpClient } from '@angular/common/http'
 export class UsersService{
 
   constructor(private httpClient: HttpClient) { }
-  
-  users: User[] = [];
+
+  users: User[];
   usersSubject = new Subject<User[]>();
+
+  private userURL = 'https://projgl-c484f-default-rtdb.europe-west1.firebasedatabase.app/users.json';
+
 
   emitUsers() {
     this.usersSubject.next(this.users);
@@ -22,8 +25,7 @@ export class UsersService{
     // https://www.youtube.com/watch?v=uYhAfgEwNWA&feature=youtu.be
     //2h58 tuto fonction ajout, 3h40 important
   
-  this.httpClient.put('https://projgl-c484f-default-rtdb.europe-west1.firebasedatabase.app/users.json',
-   this.users)
+  this.httpClient.put(this.userURL, this.users)
     .subscribe(
       () => {
         console.log('Enregistrement terminé')
@@ -35,17 +37,37 @@ export class UsersService{
   }
 
   createNewUser(newUser: User) {
+    console.log(this.users);
     this.users.push(newUser);
     this.saveUsers();
     this.emitUsers();
   }
 
-  getUsersFromServer() {
+  modifyUser(u: User) {
+    let index = this.users.findIndex(user => user.email === u.email)
+    this.users[index] = u;
+    console.log("user :")
+    console.log(this.users[index]);
+    this.saveUsers();
+    this.emitUsers();
+  }
+
+  getUsersFromServer() { //This need to be called before using function that use firebase DB
     this.httpClient
-    .get<any[]>('https://projgl-c484f-default-rtdb.europe-west1.firebasedatabase.app/users.json')
-    .subscribe(
+    .get<any[]>(this.userURL).subscribe(
       (response) => {
+        if (response == null){ // if response is null we create a 1st user to instanciate the list
+          let s = 'Collaborateur';
+          let str = [s];
+          str.push(s) ;
+          let d = new Date();
+          let us = new User('email','prenom','nom','cadre',str, d);
+          this.users = [us];
+          this.saveUsers();
+
+        }else{
         this.users = response;
+        }
         this.emitUsers();
         console.log('Chargement terminé ! ');
       },
@@ -53,8 +75,14 @@ export class UsersService{
         console.log('Erreur de chargement ! ' + error);
       }
     )
+  }
 
+  searchUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.userURL);
+  }
 
+  searchUser(): Promise<User[]> {
+        return this.httpClient.get<User[]>(this.userURL).toPromise();
   }
 
 }
